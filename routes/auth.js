e0ea744b3198d05724017fa3f99f42a5a1483fa6
john smilga <johnsmilga@gmail.com>
2024-04-15 14:52:50 -0400

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const authenticateUser = require('../middleware/authentication');
+const testUser = require('../middleware/testUser');
+
+const rateLimiter = require('express-rate-limit');
+
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: 'Too many requests from this IP, please try again after 15 minutes',
+  },
+});
+
+const {
+  register,
+  login,
+  updateUser,
+  testingRegister,
+} = require('../controllers/auth');
+router.post('/testingRegister', testingRegister);
+router.post('/register', apiLimiter, register);
+router.post('/login', apiLimiter, login);
+router.patch('/updateUser', authenticateUser, testUser, updateUser);
+
+module.exports = router;
